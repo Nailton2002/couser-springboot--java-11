1da900450030c5e52d3ad5c5c81d52a7397c780c
Nailton2002 <jnmp25jnmp@gmail.com>
2020-09-09 16:27:13 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resource/UserResource.java
+++ src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,20 +1,38 @@
 package com.educandoweb.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")//CONTROLADOR WEST QUE RESPONDE NO CAMINHO USER
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> FindAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> FindAll(){
+		//User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "123456");
+		List<User> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
+	}
+	
+	//ISTO INDICARA MINHA REQUISIÇÃO IRA ACEITAR UM ID DENTRO URL
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
